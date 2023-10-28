086b720175aeefac091709cb8b3e0219f3732c0f
Francesco Rapetti <rapettif80@yahoo.it>
2023-10-28 18:12:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+let nome = document.getElementById("name-surname");
+let kilometri = document.getElementById('km');
+let eta = document.getElementById('age');
+let btn1 = document.querySelector("#generate");
+let btn2 = document.getElementById("abort");
+const prezzoAlKm = 0.21;
+let prezzo;
+let sconto;
+let message = document.getElementById('message');
+
+// console.log(nome.value);
+// console.log(kilometri.value);
+// console.log(eta.value);
+
+btn1.addEventListener("click", function() {
+    switch (eta.value) {
+        case 'minor': sconto = 20; break;
+        case 'major': sconto = 0; break;
+        case 'over65': sconto = 40; break;
+    }
+    message.innerHTML = nome.value + kilometri.value + eta.value + sconto;
+    message.className = "d-block";
+});
+
+btn2.addEventListener("click", function() {
+    nome.value = '';
+    kilometri.value = '';
+    eta.value = 'none';
+    message.className = "d-none";
+})
+
+
+console.log(message.value);
+
+console.log(nome.value);
+console.log(kilometri.value);
+console.log(eta.value);
+
+// checks if there are char in the string
+function numberController(input) {
+    for (let index = 0; index < input.length; index++) {
+        if (isNaN(input[index])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// returns percentage
+function percentageCalculator(num, percentage) {
+    return (num / 100) * percentage;
+}
